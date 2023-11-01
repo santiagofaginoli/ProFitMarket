4711b258c312fa342844f508097b9ebd2975b7d4
Benjaminoyarzun <benjaminoyarzunlopez@iresm.edu.ar>
2023-11-01 15:34:02 -0300

    1/11 reservas listo  revisar

--- hotel/components/BookingSys.js
+++ hotel/components/BookingSys.js
@@ -1,43 +1,56 @@
-import React, { useState, useEffect } from 'react';
-import { Button, Spacer,Table, TableHeader, TableBody, TableColumn, TableRow, TableCell } from '@nextui-org/react';
+import React, { useState } from 'react';
+import { Button, Spacer, Table, TableHeader, TableBody, TableColumn, TableRow, TableCell } from '@nextui-org/react';
 
 export const Bookingsys = () => {
+  const [fechaInicio, setFechaInicio] = useState('');
+  const [fechaFin, setFechaFin] = useState('');
   const [cantidadPersonas, setCantidadPersonas] = useState('');
-  const [resultado, setResultado] = useState('');
+  const [habitacionElegida, setHabitacionElegida] = useState('');
   const [reservas, setReservas] = useState([]);
-  const [habitacionActual, setHabitacionActual] = useState(1); // Inicializa el número de habitación en 1
+  const [mensajeError, setMensajeError] = useState('');
 
   const agregarReserva = () => {
-    const cantidadPersonasInput = document.getElementById('cantidad-personas');
-    const cantidadPersonas = cantidadPersonasInput.value;
+    if (!fechaInicio || !fechaFin || !cantidadPersonas || !habitacionElegida) {
+      setMensajeError('Por favor, complete todos los campos antes de registrar la reserva.');
+      return;
+    }
 
-    // Crea un objeto de reserva
+    // Verificar si la habitación está ocupada en el rango de fechas
+    if (habitacionOcupada(habitacionElegida, fechaInicio, fechaFin)) {
+      setMensajeError('La habitación está ocupada en ese rango de fechas. Por favor, elija otras fechas o habitación.');
+      return;
+    }
+
     const nuevaReserva = {
-      id: reservas.length + 1, // Puedes usar otro método para generar IDs únicos
-      fechaInicio: document.getElementById('fecha-inicio').value,
-      fechaFin: document.getElementById('fecha-fin').value,
-      habitacion: habitacionActual, // Usa el número de habitación actual
+      id: reservas.length + 1,
+      fechaInicio: fechaInicio,
+      fechaFin: fechaFin,
+      habitacion: habitacionElegida,
       cantidadPersonas: cantidadPersonas,
     };
 
-    // Incrementa el número de habitación actual para la próxima reserva
-    setHabitacionActual(habitacionActual + 1);
-
-    // Agrega la nueva reserva al estado de reservas
     setReservas([...reservas, nuevaReserva]);
+    setMensajeError('');
+  };
 
-    // Muestra el valor en el elemento resultado
-    setResultado(`Reserva creada para ${cantidadPersonas} personas en la habitación ${nuevaReserva.habitacion}`);
+  // Función para verificar si la habitación está ocupada en el rango de fechas
+  const habitacionOcupada = (habitacion, fechaInicio, fechaFin) => {
+    return reservas.some((reserva) => {
+      return (
+        reserva.habitacion === habitacion &&
+        ((fechaInicio >= reserva.fechaInicio && fechaInicio <= reserva.fechaFin) ||
+          (fechaFin >= reserva.fechaInicio && fechaFin <= reserva.fechaFin))
+      );
+    });
   };
 
   return (
     <div>
-      <p>aaaa</p>
       <div>
         <label htmlFor="fecha-inicio" className="mr-2">
           Fecha de inicio:
         </label>
-        <input type="date" id="fecha-inicio" name="fecha-inicio" required className="rounded-lg p-2" />
+        <input type="date" id="fecha-inicio" name="fecha-inicio" value={fechaInicio} onChange={(e) => setFechaInicio(e.target.value)} required className="rounded-lg p-2" />
       </div>
       <Spacer y={2} />
 
@@ -45,7 +58,7 @@ export const Bookingsys = () => {
         <label htmlFor="fecha-fin" className="mr-2">
           Fecha de fin:
         </label>
-        <input type="date" id="fecha-fin" name="fecha-fin" required className="rounded-lg p-2" />
+        <input type="date" id="fecha-fin" name= "fecha-fin" value={fechaFin} onChange={(e) => setFechaFin(e.target.value)} required className="rounded-lg p-2" />
       </div>
       <Spacer y={2} />
 
@@ -53,35 +66,44 @@ export const Bookingsys = () => {
         <label htmlFor="cantidad-personas" className="mr-2">
           Cantidad de personas:
         </label>
-        <input type="number" id="cantidad-personas" name="cantidad-personas" min="1" max="5" required className="rounded-lg p-2" />
+        <input type="number" id="cantidad-personas" name="cantidad-personas" value={cantidadPersonas} onChange={(e) => setCantidadPersonas(e.target.value)} min="1" max="5" required className="rounded-lg p-2" />
+      </div>
+      <Spacer y={2} />
+      <div>
+        <label htmlFor="habitacion-elegida" className="mr-2">
+          Número de Habitación:
+        </label>
+        <input type="number" id="habitacion-elegida" name="habitacion-elegida" value={habitacionElegida} onChange={(e) => setHabitacionElegida(e.target.value)} min="1" max="51" required className="rounded-lg p-2" />
       </div>
       <Button id="mostrar-boton" type="submit" color="primary" onClick={agregarReserva}>
         Reservar
       </Button>
 
       <div>
-        <h2 className='text-center font-bold'>Reservas</h2>
-        <Table aria-label="Reservas table">
-          <TableHeader>
-            <TableColumn>ID</TableColumn>
-            <TableColumn>Fecha de Inicio</TableColumn>
-            <TableColumn>Fecha de Fin</TableColumn>
-            <TableColumn>Habitación</TableColumn>
-            <TableColumn>Cantidad de Personas</TableColumn>
-          </TableHeader>
-          <TableBody>
-            {reservas.map((reserva) => (
-              <TableRow key={reserva.id}>
-                <TableCell>{reserva.id}</TableCell>
-                <TableCell>{reserva.fechaInicio}</TableCell>
-                <TableCell>{reserva.fechaFin}</TableCell>
-                <TableCell>{reserva.habitacion}</TableCell>
-                <TableCell>{reserva.cantidadPersonas}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
+        {mensajeError && <div className="text-red-500">{mensajeError}</div>}
       </div>
+
+      <h2 className='text-center font-bold'>Reservas</h2>
+      <Table aria-label="Reservas table">
+        <TableHeader>
+          <TableColumn>ID</TableColumn>
+          <TableColumn>Fecha de Inicio</TableColumn>
+          <TableColumn>Fecha de Fin</TableColumn>
+          <TableColumn>Habitación</TableColumn>
+          <TableColumn>Cantidad de Personas</TableColumn>
+        </TableHeader>
+        <TableBody>
+          {reservas.map((reserva) => (
+            <TableRow key={reserva.id}>
+              <TableCell>{reserva.id}</TableCell>
+              <TableCell>{reserva.fechaInicio}</TableCell>
+              <TableCell>{reserva.fechaFin}</TableCell>
+              <TableCell>{reserva.habitacion}</TableCell>
+              <TableCell>{reserva.cantidadPersonas}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
     </div>
   );
 };
