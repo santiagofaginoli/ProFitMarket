3748c3873c3d4930456bbe0d6a60e3143a7c1133
santiagofaginoli <102366056+santiagofaginoli@users.noreply.github.com>
2024-04-16 01:38:14 -0300

    Add files via upload

--- hotel/layouts/default.tsx
+++ hotel/layouts/default.tsx
@@ -1,4 +1,4 @@
-import { Navbar } from "@/components/navbar";
+import { NavbarMain } from "@/components/navbar";
 import { Link } from "@nextui-org/link";
 import { Head } from "./head";
 
@@ -10,19 +10,20 @@ export default function DefaultLayout({
 	return (
 		<div className="relative flex flex-col h-screen">
 			<Head />
-			<Navbar />
-			<main className="container mx-auto max-w-7xl px-6 flex-grow">
+			<NavbarMain />
+			<main className="container mx-auto max-w-7xl flex-grow">
 				{children}
 			</main>
 			<footer className="w-full flex items-center justify-center py-3">
 				<Link
 					isExternal
 					className="flex items-center gap-1 text-current"
-					href="https://nextui-docs-v2.vercel.app?utm_source=next-app-template"
-					title="nextui.org homepage"
+					href="https://divgeeks-ccqalgxj0-divgeeks.vercel.app/contacto/contacto"
+					title="DivGeeks homepage"
+					target="_blank"
 				>
 					<span className="text-default-600">Powered by</span>
-					<p className="text-primary">NextUI</p>
+					<p className="text-primary">DivGeeks</p>
 				</Link>
 			</footer>
 		</div>
